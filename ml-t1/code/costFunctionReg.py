d26a850d620b73ac509ba136bfd0acaf9e051b42
Eduardo Bezerra <edubezerra@gmail.com>
2021-03-30 17:52:08 -0300

    Update costFunctionReg.py

--- ml-t1/code/costFunctionReg.py
+++ ml-t1/code/costFunctionReg.py
@@ -1,34 +1,11 @@
 def costFunctionReg(theta, X, y, lambda_):
-    '''
-    Método que calcula e atribui os valores de theta
-    '''
-    m = len(y)
-    
-    theta_shape = theta.shape[0]
-    
-    # atualização de theta
-    for theta_index in range(theta_shape):
-        value = 0
-        for line in range(m):
-            z = sigmoide(theta.T.dot(X[line]))
-            value += (z - y[line]) * X[line][theta_index]
-
-            if theta_index > 0:
-                value += (lambda_/m) * theta[theta_index]
-
-        theta[theta_index] = value
-    
-    J = custoRegLog_Norm(theta, X, y, lambda_)
-
-    return theta
-
-def custoRegLog_Norm(theta, X, y, lambda_):
-    '''
-    Função de custo da regressão logística regularizada
-    '''
-    m = len(y)
-    # Cálculo do custo
-    cost_last_component = (lambda_ / (2*m)) * (np.sum(theta ** 2))
-    J = custo_reglog(theta, X, y) +  cost_last_component
-    
-    return J
+  m = y.size #número de interações
+  J = 0
+  grad = np.zeros(theta.shape)
+  h = sigmoide(X.dot(theta.T))
+  temp = theta
+  temp[0] = 0
+  J = (1 / m) * np.sum(-y.dot(np.log(h)) - (1 - y).dot(np.log(1 - h))) + (lambda_ / (2 * m)) * np.sum(np.square(temp))
+  grad = (1 / m) * (h - y).dot(X)
+  grad = grad + (lambda_ / m) * temp
+  return J, grad
